8d8eb5a2f5adf34aca9363609ee17669166ce19e
Filipe Lopes <filiperocklopes@gmail.com>
2019-03-15 09:51:10 -0300

    Issue #3 Alerta da falta de cadastro de uma guia quando ela não for cadastrada - resolvido

--- ajax/verificar-informacoes.php
+++ ajax/verificar-informacoes.php
@@ -9,49 +9,62 @@ $guia = $_POST["guia"];
 
 if($guia != ""){
     $pesquisaguia = $mydb->query("SELECT * FROM guias WHERE numero = $guia");
+    
+    if($pesquisaguia->num_rows > 0){ // Verifica se a guia existe nos cadastros
 
-    while ($row = $pesquisaguia->fetch_assoc()) {
+        while ($row = $pesquisaguia->fetch_assoc()) {
 
-        $dentista = $row["dentista"];
-        $atendido = $row["atendido"];
-        $paciente = $row["paciente"];
-        $atendimento = $row["atendimento"];
-        $valor = $row["valor"];
-        $descricao = $row["descricao"];
-
-    }
-
-    if($atendido == 0){ //verifica se nao foi atendido
-
-        if($dentista == $_SESSION["id_usuario"]){ //verifica dentista
-
-        $sql = "UPDATE guias SET verificado = '1' WHERE numero = '$guia' ";
-        $mydb->query($sql);
-
-        $pesquisaguia_atual = $mydb->query("SELECT * FROM guias WHERE numero = '$guia' LIMIT 1");
-
-        while ($row = $pesquisaguia_atual->fetch_assoc()) {
-
-            $resposta = array(
-                "tipo" => "sucesso",
-                "mensagem" => "Verifique os dados mostrados e confirme o início da consulta",
-                "class" => "green",
-                "paciente" => $paciente,
-                "atendimento" =>  get_atendimento($atendimento)[0],
-                "valor" =>  $valor,
-                "descricao" =>  $descricao,
-                "dentista" => get_info_perfil($dentista, "nome"),
-            );
+            $dentista = $row["dentista"];
+            $atendido = $row["atendido"];
+            $paciente = $row["paciente"];
+            $atendimento = $row["atendimento"];
+            $valor = $row["valor"];
+            $descricao = $row["descricao"];
 
         }
 
-        echo json_encode($resposta);
+        if($atendido == 0){ //verifica se nao foi atendido
+
+            if($dentista == $_SESSION["id_usuario"]){ //verifica dentista
+
+            $sql = "UPDATE guias SET verificado = '1' WHERE numero = '$guia' ";
+            $mydb->query($sql);
+
+            $pesquisaguia_atual = $mydb->query("SELECT * FROM guias WHERE numero = '$guia' LIMIT 1");
+
+            while ($row = $pesquisaguia_atual->fetch_assoc()) {
+
+                $resposta = array(
+                    "tipo" => "sucesso",
+                    "mensagem" => "Verifique os dados mostrados e confirme o início da consulta",
+                    "class" => "green",
+                    "paciente" => $paciente,
+                    "atendimento" =>  get_atendimento($atendimento)[0],
+                    "valor" =>  $valor,
+                    "descricao" =>  $descricao,
+                    "dentista" => get_info_perfil($dentista, "nome"),
+                );
+
+            }
+
+            echo json_encode($resposta);
+
+            }else{
+
+                $resposta = array(
+                    "tipo" => "erro",
+                    "mensagem" => "Você não foi o dentista cadastrado para atender esse paciente",
+                    "class" => "red"
+                );
+                
+                echo json_encode($resposta);
+            }
 
         }else{
 
             $resposta = array(
                 "tipo" => "erro",
-                "mensagem" => "Você não foi o dentista cadastrado para atender esse paciente",
+                "mensagem" => "Esse paciente já foi atendido",
                 "class" => "red"
             );
             
@@ -62,11 +75,11 @@ if($guia != ""){
 
         $resposta = array(
             "tipo" => "erro",
-            "mensagem" => "Esse paciente já foi atendido",
+            "mensagem" => "Essa guia não foi cadastrada pelo(a) atendente",
             "class" => "red"
         );
         
-        echo json_encode($resposta);
+        echo json_encode($resposta);        
     }
 
 }else{
