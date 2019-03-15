87692406e5f62812586b8fcccbde2b45b9aeaafe
Filipe Lopes <filiperocklopes@gmail.com>
2019-03-15 09:43:34 -0300

    Issue #4 - Botão INICIAR ATENDIMENTO em dentista não funciona #4resolvido

--- ajax/atendimento-iniciar.php
+++ ajax/atendimento-iniciar.php
@@ -8,7 +8,7 @@ $guia = $_POST["guia"];
 if($guia != ""){
     $pesquisaguia_verificada = $mydb->query("SELECT * FROM guias WHERE numero = $guia AND verificado = '1' ");
 
-    echo $pesquisaguia_verificada->num_rows;
+    // echo $pesquisaguia_verificada->num_rows;
 
     if($pesquisaguia_verificada->num_rows){
         $sql = "UPDATE guias SET atendido = '1' WHERE numero = '$guia' ";
