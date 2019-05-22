45dbf29fc7c01a319797e076db2de1041dd7e889
Filipe Lopes <filiperocklopes@gmail.com>
2019-05-22 19:07:29 -0300

    Adicionado mensagem de 'funcionalidades em breve'

--- principal.php
+++ principal.php
@@ -25,25 +25,37 @@ include "parts/structure/head.php";
             <div class="col-sm-10 col-md-9 pt-5">
                 <div class="container row">
                     <div class="col-sm-12 col-md-9 mb-5">
-                        <h1>Olá <?=get_info_perfil($idusuario, "nome")?>
-                        <span class="d-block small font-weight-light font-italic"><?=get_info_perfil($idusuario, "funcao")?></span>
-                        </h1>
-                        <ul id="ulnav" class="col-12">                        
-                            <?php
-                            if(isFuncao($idusuario, "dentista")){ ?>
-                            <li class="mb-5"><a href="iniciar-atendimento"><span class="oi oi-play-circle"></span> Iniciar Atendimento</a></li>
-                            <li><a href="meus-atendimentos"><span class="oi oi-list"></span> Meus atendimentos</a></li>
-                            <?php }else if(isFuncao($idusuario, "administrador")){ 
-                                if(num_dentistas()[1] > 0){ ?>
-                                <li><a href="validacao-cadastro"><span class="oi oi-task"></span> Você tem <?=num_dentistas()[1]?> cadastro(s) pendentes para validar!</a></li>
-                                <?php }?>
-                            <li><a href="perfil"><span class="oi oi-person"></span> Meus dados cadastrais</a></li>
+                        <div class="row">
+                            <h1>Olá <?=get_info_perfil($idusuario, "nome")?>
+                            <span class="d-block small font-weight-light font-italic"><?=get_info_perfil($idusuario, "funcao")?></span>
+                            </h1>
+                            <ul id="ulnav" class="col-12">                        
+                                <?php
+                                if(isFuncao($idusuario, "dentista")){ ?>
+                                <li class="mb-5"><a href="iniciar-atendimento"><span class="oi oi-play-circle"></span> Iniciar Atendimento</a></li>
+                                <li><a href="meus-atendimentos"><span class="oi oi-list"></span> Meus atendimentos</a></li>
+                                <?php }else if(isFuncao($idusuario, "administrador")){ 
+                                    if(num_dentistas()[1] > 0){ ?>
+                                    <li><a href="validacao-cadastro"><span class="oi oi-task"></span> Você tem <?=num_dentistas()[1]?> cadastro(s) pendentes para validar!</a></li>
+                                    <?php }?>
+                                <li><a href="perfil"><span class="oi oi-person"></span> Meus dados cadastrais</a></li>
+                                <?php } ?>
+                            </ul>
+                        </div>
+                        
+                        <!-- Relatórios e gráficos para administrador -->
+                        <div class="row">
+                            <?php if(isFuncao($idusuario, "administrador")){ ?>
+                            <div class="col-md-12 mb-5">
+                                Em breve gráfico de ganhos com atalho para relatórios
+                            </div>
                             <?php } ?>
-                        </ul>
+                        </div>
+
                     </div>
                     <?php if(isFuncao($idusuario, "administrador")){ ?>
                         <div class="col-sm-12 col-md-3 mb-5">
-                            menu
+                            Em breve uma lista das 5 últimas guias pagas
                         </div>
                     <?php } ?>
                 </div>
