01619ccac4d1c425bdf130ac7004be98888ee1f5
Filipe Lopes <filiperocklopes@gmail.com>
2019-07-23 12:13:14 -0300

    Adicionado gráfico de valores esperados e recebidos nos últimos 5 meses

--- principal.php
+++ principal.php
@@ -46,8 +46,104 @@ include "parts/structure/head.php";
                         <!-- Relatórios e gráficos para administrador -->
                         <div class="row">
                             <?php if(isFuncao($idusuario, "administrador")){ ?>
+                            <?php
+
+                            $meses = array(
+                                "", "Janeiro",
+                                "Fevereiro",
+                                "Março",
+                                "Abril",
+                                "Maio",
+                                "Junho",
+                                "Julho",
+                                "Agosto",
+                                "Setembro",
+                                "Outubro",
+                                "Novembro",
+                                "Dezembro"
+                            );
+
+                            $meses_mostrados = array();
+                            // echo strtotime("now");
+                            // echo "<br>";
+                            // echo strtotime("+1 month");      
+                            for ($i=0; $i < 5; $i++) { 
+                                $mes = date('m', strtotime("-{$i} months"));
+                                array_push($meses_mostrados, $mes);
+                            }
+                            // print_r($meses_mostrados);
+                            sort($meses_mostrados);
+
+                            $labels = array();
+                            $valor_esperado = array();
+                            $valor_recebido = array();
+                            // $valor_repassado = array(); 
+
+                            foreach ($meses_mostrados as $mes) {
+                                $ano = date('Y');
+                                // echo $ano;
+                                array_push($labels, $meses[intval($mes)]);
+                                $s_data = $mes.'-'.$ano;
+                                // echo $s_data;
+
+                                // Pesquisa valores esperados
+                                $valor_esperado_q = $mydb->query("SELECT SUM(valor) FROM guias WHERE DATE_FORMAT(datahora, '%m-%Y') = '{$s_data}'");
+                                foreach( $valor_esperado_q as $valor ){
+                                    array_push($valor_esperado, $valor["SUM(valor)"]);
+                                }
+
+                                // Pesquisa valores recebidos
+                                $valor_recebido_q = $mydb->query("SELECT SUM(valor_pago) FROM guias WHERE DATE_FORMAT(datahora, '%m-%Y') = '{$s_data}'");
+                                foreach( $valor_recebido_q as $valor ){
+                                    array_push($valor_recebido, $valor["SUM(valor_pago)"]);
+                                }
+                            }
+
+                            // print_r($valor_esperado);
+                            // print_r($valor_recebido);
+
+
+                            
+
+                            // Separa em data de 
+
+                            
+                            ?>
                             <div class="col-md-12 mb-5">
-                                Em breve gráfico de ganhos com atalho para relatórios
+                            <h2 class="mt-4">Gráfico de valores dos últimos 5 meses</h2>
+                            <canvas id="myChart" width="400" height="200"></canvas>
+                            <script>
+                            var ctx = $('#myChart');
+                            var myChart = new Chart(ctx, {
+                                type: 'line',
+                                data: {
+                                    labels: <?=json_encode($labels)?>,
+                                    datasets: [{
+                                        label: 'Valor esperado',
+                                        data: <?=json_encode($valor_esperado)?>,
+                                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                                        borderColor: '#690704',
+                                        borderWidth: 2
+                                    }, {
+                                        label: 'Valor recebido',
+                                        data: <?=json_encode($valor_recebido)?>,
+                                        backgroundColor: 'rgba(50, 219, 140, 0.2)',
+                                        borderColor: '#30ad71',
+                                        borderWidth: 2
+                                    }]
+                                },
+                                options: {
+                                    scales: {
+                                        yAxes: [{
+                                            ticks: {
+                                                beginAtZero: true
+                                            }
+                                        }]
+                                    }
+                                }
+                            });
+                            </script>
+                                Em breve gráfico adicional de Atendimentos Realizados
                             </div>
                             <?php } ?>
                         </div>
